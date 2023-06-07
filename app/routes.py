2e2f533baa7df85fa5ce71faeaa9e1c9fc79c6ea
JakubZajas <225983@student.uek.krakow.pl>
2023-06-07 12:59:49 +0200

    2023-06-07 JZ

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,27 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return " Hello Jakub Zajas"
+    return render_template("index.html")
 
-@app.route('/name/',defaults={'name': None})
-@app.route('/name/<name>')
-def name(name=None):
-    return f"Hello {name}"
+@app.route('/')
+@app.route('/extract')
+def extract():
+    return render_template("extract.html")
+
+@app.route('/')
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.route('/')
+@app.route('/product')
+def product():
+    return render_template("product.html")
+
+@app.route('/')
+@app.route('/author')
+def author():
+    return render_template("author.html")
